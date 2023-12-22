a333c48c78d006982b45502b2f81eeb56bd75fb8
root <root@ip-172-31-15-198.us-west-1.compute.internal>
2023-12-22 12:15:12 +0000

    newcommit

---
+++ contact.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+try{
+$con=mysqli_connect("localhost","admin","admin123","phpdb");
+}catch(Exception $e){
+ echo $e->getMessage();
+}
+
+if(isset($_POST['btnSubmit']))
+{
+	$name = $_POST['txtName'];
+	$email = $_POST['txtEmail'];
+	$contact = $_POST['txtPhone'];
+	$message = $_POST['txtMsg'];
+
+	$query="insert into contact(name,email,contact,message) values('$name','$email','$contact','$message');";
+	$result = mysqli_query($con,$query);
+
+	if($result)
+    {
+    	echo '<script type="text/javascript">'; 
+		echo 'alert("Message sent successfully!");'; 
+		echo 'window.location.href = "contact.html";';
+		echo '</script>';
+    }
+}
